8dd8a1830990beb36719183a193a7559dbd9844f
Your Name <you@example.com>
2021-03-29 18:10:49 +0000

    comment fix

--- app.py
+++ app.py
@@ -4,12 +4,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    # get_data()
-    # a flask alapértelmezetten a templates mappában keresi a html
+    # a flask most egy egyszerű string-et ad vissza:
     return 'hello from flask'
 
 
-# ha a futtatott file, az éppen ez a file
+# ha a futtatott file, az éppen ez a file akkor induljon el
+# az alkalmazás dev mode-ban, az 5000-es porton
 if __name__ == '__main__':
     app.debug = True
     # akkor elindul a flask

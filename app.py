8e6d5257686685490c85677926335961f9b06053
Your Name <you@example.com>
2021-03-29 17:25:07 +0000

    gitignore

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, redirect, url_for, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    # get_data()
+    # a flask alapértelmezetten a templates mappában keresi a html
+    return 'hello'
+
+
+# ha a futtatott file, az éppen ez a file
+if __name__ == '__main__':
+    app.debug = True
+    # akkor elindul a flask
+    app.run(host='0.0.0.0', port=5000)
